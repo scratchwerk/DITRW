cce0284e64f16a0f0c0c4017b1677dd9d8882aa7
scratchwerk <ronnie.king@gmail.com>
2013-08-11 22:55:42 -0400

    New changes

--- DITRWApp/src/com/ditrw/game/objects/Board.java
+++ DITRWApp/src/com/ditrw/game/objects/Board.java
@@ -1,16 +1,48 @@
 package com.ditrw.game.objects;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+
+import com.ditrw.game.users.Player;
 import com.google.appengine.api.rdbms.AppEngineDriver;
 
 public class Board {
-	ArrayList players = null;
+	private ArrayList players = null;
+	private int currentPlayer = 0;
 	
 	//board questions
-	ArrayList group1Questions = null;
-	ArrayList group2Questions = null;
-	ArrayList group3Questions = null;
-	ArrayList group4Questions = null;
+	private ArrayList group1Questions = null;
+	private ArrayList group2Questions = null;
+	private ArrayList group3Questions = null;
+	private ArrayList group4Questions = null;
 	
+	private int group1Pos = 0;
+	private int group2Pos = 0;
+	private int group3Pos = 0;
+	private int group4Pos = 0;
+	
+	
+	private HashMap outerSquares = null; 
+	private HashMap innerSquares = null; 
+	
+	
+	//move to the next player
+	public Player nextPlayer(){
+		Player player = null;
+		
+		int size = players.size();
+		
+		if(currentPlayer + 1 <= size){
+			currentPlayer++;
+			player = (Player)players.get(currentPlayer);
+		}else{
+			currentPlayer = 0;
+			player = (Player)players.get(currentPlayer);
+		}
+		
+		return player;
+	}
+	
+	//pick a 
 	
 }
--- DITRWApp/src/com/ditrw/game/objects/Question.java
+++ DITRWApp/src/com/ditrw/game/objects/Question.java
@@ -1,5 +1,22 @@
 package com.ditrw.game.objects;
 
 public class Question {
-
+	private String question = null;
+	private String answer = null;
+	
+	
+	public String getQuestion() {
+		return question;
+	}
+	public void setQuestion(String question) {
+		this.question = question;
+	}
+	public String getAnswer() {
+		return answer;
+	}
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+	
+	
 }
