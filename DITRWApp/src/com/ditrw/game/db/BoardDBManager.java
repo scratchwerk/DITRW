c7e3b1ce87ad01d6630deb8b5f656c7bf25408f4
scratchwerk <ronnie.king@gmail.com>
2013-09-04 22:57:36 -0400

    Update on J2EE game

--- DITRWApp/src/com/ditrw/game/db/BoardDBManager.java
+++ DITRWApp/src/com/ditrw/game/db/BoardDBManager.java
@@ -13,9 +13,11 @@ import com.google.appengine.api.datastore.Query.FilterOperator;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 
 import com.ditrw.game.objects.Board;
+import com.ditrw.game.objects.BoardSquare;
 
 public class BoardDBManager {
 
@@ -27,7 +29,15 @@ public class BoardDBManager {
 			DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
 			Query query = new Query("BoardSquare");
 			List<Entity> boardSquares = datastore.prepare(query).asList(FetchOptions.Builder.withLimit(10));
-			
+			Iterator it = boardSquares.iterator();
+			while(it.hasNext()){
+				BoardSquare bs = new BoardSquare();
+				Entity et = (Entity)it.next();
+				bs.setSquareName((String) et.getProperty("squareName"));
+				bs.setSquarePosition((Integer) et.getProperty("squarePosition"));
+				bs.setSquareType((Integer) et.getProperty("squareType"));
+				squares.put(bs.getSquarePosition(), bs);
+			}
 		}catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
